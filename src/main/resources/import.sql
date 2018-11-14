d49b27a10ad89f84d824cabb4653e543a1773774
Abbas Wadiwala <AWadiwala92@gmail.com>
2018-11-14 14:01:07 +0000

    javax persistence object initial commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO Account (accountNo, firstname, surname) VALUES (0,'Abbas','Wadiwala');
+INSERT INTO Account (accountNo, firstname, surname) VALUES (1,'Rohan','Maini');
+INSERT INTO Account (accountNo, firstname, surname) VALUES (2,'Michal','SilentC');
+ 
